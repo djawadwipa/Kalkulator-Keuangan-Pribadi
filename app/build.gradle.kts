999fb3d0336e68ff9b658d96ec5a12e24f76fddb
djawadwipa <fanyagung@gmail.com>
2026-08-01 14:09:18 +0700

    chore(release): bump application to 0.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "id.djawadwipa.kalkulatorkeuangan"
         minSdk = 23
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0"
+        versionCode = 4
+        versionName = "0.4.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         vectorDrawables {
