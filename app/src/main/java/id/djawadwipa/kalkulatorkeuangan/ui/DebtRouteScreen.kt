85b832af029265fb66b1046550b645109ac2c4dc
djawadwipa <fanyagung@gmail.com>
2026-08-01 16:20:48 +0700

    fix(debt): use ColumnScope weight without internal import

--- app/src/main/java/id/djawadwipa/kalkulatorkeuangan/ui/DebtRouteScreen.kt
+++ app/src/main/java/id/djawadwipa/kalkulatorkeuangan/ui/DebtRouteScreen.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
