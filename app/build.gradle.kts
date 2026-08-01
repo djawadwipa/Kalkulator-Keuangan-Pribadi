5035ad770c7ce5cd804ac406c8e09b728ccefe24
djawadwipa <fanyagung@gmail.com>
2026-08-01 16:22:19 +0700

    chore(release): bump app version to 0.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "id.djawadwipa.kalkulatorkeuangan"
         minSdk = 23
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.5.0"
+        versionCode = 6
+        versionName = "0.6.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         vectorDrawables {
