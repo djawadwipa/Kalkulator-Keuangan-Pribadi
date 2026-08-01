28fe3d90eec3f081a93a098a5a1ce1bd7a076fcf
djawadwipa <fanyagung@gmail.com>
2026-08-02 03:16:01 +0700

    fix: make master cards fully clickable (#20)
    Co-authored-by: djawadwipa <245683147+djawadwipa@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "id.djawadwipa.kalkulatorkeuangan"
         minSdk = 23
         targetSdk = 36
-        versionCode = 10
-        versionName = "0.9.1"
+        versionCode = 11
+        versionName = "0.9.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         vectorDrawables {
